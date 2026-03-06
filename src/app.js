532d35966ade561b84be5fc94c1a071b70ea5a89
pedagawo <96032352+pedagawo@users.noreply.github.com>
2026-03-05 20:39:55 -0600

    Commit inicial

---
+++ src/app.js
@@ -0,0 +1,16 @@
+// src/app.js
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'API Lab06 Activa', version: '1.0.0'});
+});
+
+// EndPoint a verificar por el pipeline post deploy
+app.get('/health', (req, res) => {
+    res.json({ status: 'ok', timestamp: new Date().toISOString()});
+});
+
+module.exports = app;
---
+++ tests/app.test.js
@@ -0,0 +1,22 @@
+// tests/app.test.js
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('Lab06 API', () => {
+    test('GET / responde con mensaje de bienvenida', async () => {
+        const res = await request(app).get('/');
+        expect(res.statusCode).toBe(200);
+        expect(res.body.message).toBe('API Lab06 Activa');
+    });
+
+    test('GET /health retorna estado ok', async () => {
+        const res = await request(app).get('/health');
+        expect(res.statusCode).toBe(200);
+        expect(res.body.status).toBe('ok');
+    });
+
+    test('Incluye versión del app', async () => {
+        const res = await request(app).get('/');
+        expect(res.body).toHaveProperty('version');
+    })
+})
