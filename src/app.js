3bf96df5adf85083f23c997ae6a2a7d7291f22b5
Jonatán Chávez <jonatanlemuelchavezserrano@gmail.com>
2026-03-06 19:40:06 -0600

    Personalizar mensaje con carnet 118***3

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({ message: 'API Lab06 Activa', version: '1.0.0'});
+    res.json({ message: 'API Carnet: 118***3', version: '1.0.0'});
 });
 
 // EndPoint a verificar por el pipeline post deploy
