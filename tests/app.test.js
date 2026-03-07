baee408e11f94eb2e375473d11067b721c2e0377
harriett 3 <HGUZMAN@correo.url.edu.gt>
2026-03-06 19:37:51 -0600

    FINALIZADO

--- tests/app.test.js
+++ tests/app.test.js
@@ -6,7 +6,7 @@ describe('Lab06 API', () => {
     test('GET / responde con mensaje de bienvenida', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toBe(200);
-        expect(res.body.message).toBe('API Lab06 Activa');
+        expect(res.body.message).toBe('API Lab06 Activa 1112423 - Harriett Guzmán');
     });
 
     test('GET /health retorna estado ok', async () => {
